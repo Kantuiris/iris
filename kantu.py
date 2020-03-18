dc4777c945eff5ce978adf1bbe2caed46708fd76
Iris <kantuiris123@gmail.com>
2020-03-18 16:05:24 +0300

    my trial

--- kantu.py
+++ kantu.py
@@ -12,12 +12,27 @@
 # else:
     # print("failing grade")
 
-grade=60
-    if grade>=85:
-        print("A")
-    elif grade>=70:
-        print("B")
-    elif grade>=65:
-        print("C")
-    else grade<=60:
-         print("failing grade")
+# grade=60
+# if grade>=85:
+    # print("A")
+# elif grade>=70:
+    # print("B")
+# elif grade>=65:
+    # print("C")
+# else:
+    # print("failing grade")
+
+# grade=50
+# if grade>=60:
+    # print ("condition is true")
+# else:
+    # if grade>=55:
+    #    print ("checking second value")
+    # else:
+        # print ("condition is false")
+
+# a=10
+# while a>0:
+    # print('value of a is')
+    # a=a-2
+# print ("loop is complete")
